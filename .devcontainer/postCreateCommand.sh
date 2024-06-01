2071773675741c66f248c332493ec19fe51450ad
destinyfvcker <destinyfunk@163.com>
2024-06-01 12:12:15 +0800

    update some config

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-git submodule update --remote --init --recursive --depth=20
+git submodule update --init --recursive --depth=20
+# git submodule update --remote
 rustup override set $(`dirname $0`/../linux_raspberrypi/scripts/min-tool-version.sh rustc)
 rustup component add rust-src
 cargo install --locked --version $(`dirname $0`/../linux_raspberrypi/scripts/min-tool-version.sh bindgen) bindgen-cli
